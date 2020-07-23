4b7b4ee1a2cd00c10d754788dbfd30e1e5a6f02f
Matías Lorenzo <mlorenzo@xmartlabs.com>
2020-07-23 14:41:41 -0300

    updates ApiError's wrong prototype fix

--- template/src/models/api-error.js
+++ template/src/models/api-error.js
@@ -9,30 +9,22 @@ const ERROR_STATUSES = {
   SERVER_ERROR: 500,
 };
 
-/*
-  We need this identifier since Babel and other transpilers have trouble
-  setting prototypes when extending native classes like `Error`. You can read
-  more here: https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
-
-  Since we want to support somewhat older browsers (IE 10 and 9) we can't use
-  any known workarounds for this, so we manually set a name on the error we can use.
-*/
-const ERROR_NAME = 'ApiError';
-
 class ApiError extends Error {
-  /*
-    Checks that an error is of this type. Check the comment above
-    for more info on why this is done this way.
-  */
-  static isApiError(error) {
-    return error.name === ERROR_NAME;
-  }
-
   constructor(params) {
     super(params.message);
     this.status = params.status;
     this.code = params.code;
-    this.name = ERROR_NAME;
+    /*
+      We need to set the prototype manually since Babel and other transpilers have trouble
+      setting prototypes when extending native classes like `Error`. You can read
+      more here: https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work.
+      That documentation is of Typescript, but seems like the issue is from Babel,
+      not Typescript itself.
+
+      Since we want to support somewhat older browsers (IE 11 for instance) we can't use
+      any known workarounds for this, so we manually set a name on the error we can use.
+    */
+    Object.setPrototypeOf(this, ApiError.prototype);
   }
 }
 
