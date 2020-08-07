2ee4433b9392f6d9c5d2abba073da13543d794da
Matías Lorenzo <mlvieras@gmail.com>
2020-08-07 08:10:05 -0300

    moves usage of withLayout HOC to the index file on the home page

--- template/src/pages/home/index.js
+++ template/src/pages/home/index.js
@@ -1,1 +1,6 @@
-export { Home } from './home';
+import { withLayout, LAYOUT_TYPES } from '../../hocs/with-layout';
+import { Home } from './home';
+
+const WrappedHome = withLayout(LAYOUT_TYPES.HOME, Home);
+
+export { WrappedHome as Home };
