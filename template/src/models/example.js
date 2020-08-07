435211bad8f18933956790eb2c09e46bbd3e65db
Matías Lorenzo <mlvieras@gmail.com>
2020-08-07 08:14:17 -0300

    some refactor to networking boilerplate

--- template/src/models/example.js
+++ template/src/models/example.js
@@ -1,4 +1,6 @@
 /*
+  NOTE: this file serves only as an example and is not used.
+  You can remove it altogether or repurpose it by changing its name.
   Learn more about our networking architecture on:
   https://blog.xmartlabs.com/2020/07/09/frontend-architecture-and-best-practices/
 */
--- template/src/networking/api-routes.js
+++ template/src/networking/api-routes.js
@@ -1,3 +1,7 @@
+/*
+  Add all API paths used on the app here so that
+  they are centrally documented.
+*/
 const API_ROUTES = {
   EXAMPLE: '/example',
 };
