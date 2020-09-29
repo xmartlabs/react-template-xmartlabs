3a35750fbe3f6242999c75c3aa595cdf1f045e6a
Matías Lorenzo <mlorenzo@xmartlabs.com>
2020-09-29 14:22:50 -0300

    adds container sass-related code

--- template/src/pages/home/home.jsx
+++ template/src/pages/home/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import styles from './home.module.scss';
+import globalStyles from '../../assets/stylesheets/global-styles.module.scss';
 
 const Home = () => (
-  <div className={styles.container}>
+  <div className={globalStyles.genericContainer}>
     <h1 className={styles.title}>
       Welcome! This is the homepage.
     </h1>
