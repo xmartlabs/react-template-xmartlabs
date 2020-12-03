640d3b60de8e095e2cb57218714fd60a5c00084e
Matías Lorenzo <mlorenzo@xmartlabs.com>
2020-12-03 09:49:41 -0300

    adds error boundary

--- template/src/app.jsx
+++ template/src/app.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 import { Router } from './routes';
 import { routeConfig } from './route-components';
 import './index.scss';
+import { ErrorBoundary } from './common/error-boundary';
 
 const App = () => (
-  <Router routeConfig={routeConfig} />
+  <ErrorBoundary>
+    <Router routeConfig={routeConfig} />
+  </ErrorBoundary>
 );
 
 export { App };
---
+++ template/src/pages/unexpected-error/unexpected-error.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// TODO: Implement this component
+const UnexpectedError = () => (
+  <>
+    <h1>An unexpected error has occured.</h1>
+  </>
+);
+
+export { UnexpectedError };
