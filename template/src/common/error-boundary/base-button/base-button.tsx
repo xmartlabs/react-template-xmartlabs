95cff5ecb2b22ecfc4780ee6b4955b22a4968fc6
Samuel Burbano <samuel@hipecommerce.com>
2022-12-14 08:10:35 -0500

    feat: base button

--- template/src/common/error-boundary/base-button/base-button.tsx
+++ template/src/common/error-boundary/base-button/base-button.tsx
@@ -1,10 +1,10 @@
-type IProps = {
+type IProps = React.HTMLProps<HTMLButtonElement> & {
   type?: 'button' | 'submit' | 'reset';
-} | React.HTMLProps<HTMLButtonElement>;
+};
 
-export const BaseButton: React.FC<IProps> = ({ type, ...props }) => (
+export const BaseButton: React.FC<IProps> = ({ type = 'button', children = 'Button', ...props }) => (
   // eslint-disable-next-line react/jsx-props-no-spreading, react/button-has-type
-  <button type={type || 'button'} {...props}>
-    Hola
+  <button type={type} {...props}>
+    {children}
   </button>
 );
