9a32dcc7a701a12e53a61c83eb58d869aa776e38
Victor Regueira <victor1mrs@hotmail.com>
2023-02-17 16:08:35 -0300

    creating Modal component

---
+++ template/src/common/modal/modal.tsx
@@ -0,0 +1,57 @@
+import { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { classnames } from 'helpers/utils';
+import xSvg from '../../assets/icons/x.svg';
+import modalStyles from './modal.module.scss';
+
+export enum ModalSizes {
+  small = 'small',
+  medium = 'medium',
+  big = 'big',
+  unresticted = 'unresticted',
+}
+
+interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
+  size: ModalSizes
+  isOpen: boolean;
+  onClose: () => void;
+  hideCloseButton?: boolean;
+  children?: React.ReactNode;
+  modalClassName?: string;
+  ariaLabelledby: string;
+}
+
+export const Modal = ({
+  size, title, hideCloseButton = false, children, isOpen, onClose, ...props
+}: ModalProps) => {
+  const { modalClassName, ariaLabelledby } = props;
+  useEffect(() => {
+    const escFunction = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+    document.addEventListener('keydown', escFunction);
+
+    return () => {
+      document.removeEventListener('keydown', escFunction);
+    };
+  }, []);
+
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(
+    <div className={modalStyles.overlay}>
+      <div
+        role="dialog"
+        aria-labelledby={ariaLabelledby}
+        aria-modal="true"
+        className={classnames(modalStyles.modal, modalStyles[`modal-${size}`], modalClassName || '')}
+      >
+        {!hideCloseButton && <button className={modalStyles.closeButton} type="button" onClick={onClose}><img alt="x icon" src={xSvg} /></button>}
+        {children}
+      </div>
+    </div>,
+    document.getElementById('modal')!,
+  );
+};
--- template/src/pages/home/home.tsx
+++ template/src/pages/home/home.tsx
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import globalStyles from 'assets/stylesheets/global-styles.module.scss';
 import { AppLink } from 'routes/app-link';
 import { RouteName } from 'routes/routes';
-
 import { Button } from 'common/button';
 import { Avatar } from 'common/avatar';
+import { Modal } from 'common/modal';
+import { ModalSizes } from 'common/modal/modal';
 import styles from './home.module.scss';
 
-const Home = () => (
-  <div className={globalStyles.genericContainer}>
-    <h1 className={styles.title}>
-      <Avatar size="xl" />
-      <hr />
-      Welcome! This is the homepage.
-      <AppLink routeName={RouteName.About}>About</AppLink>
-      <Button>Hola Mundo</Button>
-    </h1>
-  </div>
-);
+const Home = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const closeModal: () => void = () => { setOpenModal(false); };
+  const position = styles.modalPosition;
+  return (
+    <div className={globalStyles.genericContainer}>
+      <h1 className={styles.title}>
+        <Avatar size="xl" />
+        <hr />
+        Welcome! This is the homepage.
+        <AppLink routeName={RouteName.About}>About</AppLink>
+        <Button onClick={() => setOpenModal(true)}>Hola Mundo</Button>
+      </h1>
+      {openModal && (
+        <Modal
+          isOpen={openModal}
+          onClose={closeModal}
+          size={ModalSizes.small}
+          modalClassName={position}
+          ariaLabelledby="modal-example"
+        >
+          <div className={styles.modalContent}>
+            <h3 className={styles.modalTitle}>Title</h3>
+            <span>
+              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+              turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor
+              sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies
+              mi vitae est. Mauris placerat eleifend leo.
+            </span>
+            <div className={styles.modalFooter}>
+              <Button bColor="secondary" onClick={() => setOpenModal(false)}>Cancel</Button>
+              <Button>Accept</Button>
+            </div>
+          </div>
+        </Modal>
+      )}
+    </div>
+  );
+};
 
 export { Home };
