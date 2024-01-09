826318363383a6a6f44ab7c42f1a99d3950aac14
Micaela Torres <micaela.torres@xmartlabs.com>
2024-01-09 11:22:05 -0300

    chore: update to vite 5 (#69)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-/// <reference types="vitest" />
 /// <reference types="vite/client" />
-import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,8 +25,4 @@ export default defineConfig({
       },
     }),
   ],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
 });
