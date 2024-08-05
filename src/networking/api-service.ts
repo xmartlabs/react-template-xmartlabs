97737418bfeb1d89713aa85b8f0eb3cab37bc164
Micaela Torres <micaela.torres@xmartlabs.com>
2024-08-05 09:43:00 -0300

    mend

--- src/networking/api-service.ts
+++ src/networking/api-service.ts
@@ -3,83 +3,81 @@ import { ApiError } from "./api-error";
 import { ErrorCode } from "./types/error-code";
 
 enum HttpMethod {
-  Get = "GET",
-  Post = "POST",
-  Patch = "PATCH",
-  Put = "PUT",
-  Delete = "DELETE",
+	Get = "GET",
+	Post = "POST",
+	Patch = "PATCH",
+	Put = "PUT",
+	Delete = "DELETE",
 }
 
-// NOTE: This class is intended to be used on the client only. Requests from the backend
-// to third-party tools should be done using `fetch` directly or another kind of service.
 class ApiServiceClass {
-  private addedHeaders: { [key: string]: string };
+	private addedHeaders: { [key: string]: string };
 
-  constructor() {
-    this.addedHeaders = {
-      "Content-Type": "application/json",
-    };
-  }
+	constructor() {
+		this.addedHeaders = {
+			"Content-Type": "application/json",
+		};
+	}
 
-  // NOTE: `data` is of `any` type since it's most likely an instance of `Error` or
-  // data that comes from the backend.
-  static _raiseError(data: any) {
-    throw new ApiError({
-      code: data?.code || ErrorCode.UNEXPECTED_ERROR,
-      status: data?.status,
-      message: data?.message || "An unexpected error has occurred",
-    });
-  }
+	// NOTE: `data` is of `any` type since it's most likely an instance of `Error` or
+	// data that comes from the backend.
+	static _raiseError(data: any) {
+		throw new ApiError({
+			code: data?.code || ErrorCode.UNEXPECTED_ERROR,
+			status: data?.status,
+			message: data?.message || "An unexpected error has occurred",
+		});
+	}
 
-  async _sendRequest<ReturnType>(
-    method: HttpMethod,
-    path: string,
-    config: RequestInit = {},
-  ) {
-    const updatedConfig = { ...config };
-    updatedConfig.headers = { ...this.addedHeaders, ...(config.headers || {}) };
-    const fullURL = new URL(path, constants.apiBaseURL);
-    const response = await fetch(fullURL, {
-      method,
-      ...updatedConfig,
-    });
-    let data;
-    try {
-      data = (await response.json()) as Promise<{ data: ReturnType }>;
-    } catch (error) {
-      // eslint-disable-next-line no-underscore-dangle
-      ApiServiceClass._raiseError(error);
-    }
-    if (!response.ok) {
-      // eslint-disable-next-line no-underscore-dangle
-      ApiServiceClass._raiseError(data);
-    }
-    return data;
-  }
+	async _sendRequest<ReturnType>(
+		method: HttpMethod,
+		path: string,
+		config: RequestInit = {}
+	) {
+		const updatedConfig = { ...config };
+		updatedConfig.headers = { ...this.addedHeaders, ...(config.headers || {}) };
+		const fullURL = new URL(path, constants.apiBaseURL);
+		const response = await fetch(fullURL, {
+			method,
+			...updatedConfig,
+		});
+		let data;
+		try {
+			data = (await response.json()) as Promise<{ data: ReturnType }>;
+		} catch (error) {
+			// eslint-disable-next-line no-underscore-dangle
+			ApiServiceClass._raiseError(error);
+		}
+		if (!response.ok) {
+			// eslint-disable-next-line no-underscore-dangle
+			ApiServiceClass._raiseError(data);
+		}
+		return data;
+	}
 
-  setHeaders(newHeaders: { [key: string]: string }) {
-    this.addedHeaders = { ...this.addedHeaders, ...newHeaders };
-  }
+	setHeaders(newHeaders: { [key: string]: string }) {
+		this.addedHeaders = { ...this.addedHeaders, ...newHeaders };
+	}
 
-  get<ReturnType>(path: string, config: RequestInit = {}) {
-    return this._sendRequest<ReturnType>(HttpMethod.Get, path, config);
-  }
+	get<ReturnType>(path: string, config: RequestInit = {}) {
+		return this._sendRequest<ReturnType>(HttpMethod.Get, path, config);
+	}
 
-  post<ReturnType>(path: string, config: RequestInit = {}) {
-    return this._sendRequest<ReturnType>(HttpMethod.Post, path, config);
-  }
+	post<ReturnType>(path: string, config: RequestInit = {}) {
+		return this._sendRequest<ReturnType>(HttpMethod.Post, path, config);
+	}
 
-  patch<ReturnType>(path: string, config: RequestInit = {}) {
-    return this._sendRequest<ReturnType>(HttpMethod.Patch, path, config);
-  }
+	patch<ReturnType>(path: string, config: RequestInit = {}) {
+		return this._sendRequest<ReturnType>(HttpMethod.Patch, path, config);
+	}
 
-  put<ReturnType>(path: string, config: RequestInit = {}) {
-    return this._sendRequest<ReturnType>(HttpMethod.Put, path, config);
-  }
+	put<ReturnType>(path: string, config: RequestInit = {}) {
+		return this._sendRequest<ReturnType>(HttpMethod.Put, path, config);
+	}
 
-  delete(path: string, config: RequestInit = {}) {
-    return this._sendRequest(HttpMethod.Delete, path, config);
-  }
+	delete(path: string, config: RequestInit = {}) {
+		return this._sendRequest(HttpMethod.Delete, path, config);
+	}
 }
 
 const ApiService = new ApiServiceClass();
