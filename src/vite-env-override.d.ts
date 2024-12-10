3d89e799965925c80abf4dfac4a8d951e00fbf4f
Matías Lorenzo <mlorenzo@xmartlabs.com>
2024-12-10 12:38:43 -0300

    adds comment for SVG module declaration

--- src/vite-env-override.d.ts
+++ src/vite-env-override.d.ts
@@ -40,6 +40,9 @@ declare module "*.webp" {
   export default src;
 }
 
+// NOTE: this declaration was copied from https://github.com/pd4d10/vite-plugin-svgr/blob/v4.3.0/client.d.ts.
+// It is required since the original declaration is not compatible with how we import SVGs.
+// vite-plugin-svgr expects the import to be `"icon.svg?react"` but we import it as `"icon.svg"`.
 declare module "*.svg" {
   import type * as React from "react";
 
