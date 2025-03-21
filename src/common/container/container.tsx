0d15cd91f81d5a14d1efe33c72ca214bdcfa6efc
Matías Lorenzo <mlorenzo@xmartlabs.com>
2025-03-21 16:42:22 -0300

    fixes issue with not using the cn class

--- src/common/container/container.tsx
+++ src/common/container/container.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/helpers/utils";
+
 export type ContainerProps = React.HTMLProps<HTMLDivElement> & {
   className?: string;
 };
@@ -8,7 +10,10 @@ export const Container: React.FC<ContainerProps> = ({
   ...props
 }) => (
   <div
-    className={`mx-auto w-full px-4 sm:w-[40rem] md:w-[48rem] lg:w-[64rem] xl:w-[80rem] 2xl:w-[96rem] ${className}`}
+    className={cn(
+      `2xl:w-[96rem]) mx-auto w-full px-4 sm:w-[40rem] md:w-[48rem] lg:w-[64rem] xl:w-[80rem]`,
+      className,
+    )}
     {...props}
   >
     {children}
